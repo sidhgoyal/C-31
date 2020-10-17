e4c0ceb626d2e6848a8e41b829e8560b3513920e
sidhgoyal <sidhgoyal>
2020-10-17 15:59:03 +0530

    Trajectory

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
@@ -9,5 +11,15 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.velocity.x>10 && this.body.position.x>200) {
+      var pos = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(pos);
+    }
+    
+    for (var index = 0; index < this.trajectory.length; index++) {
+      const element = this.trajectory[index];
+      image(this.smokeImage,this.trajectory[index][0],this.trajectory[index][1])
+    }
+    
   }
 }
